5edf815dd034e95272667ef35674fd7f66259087
Que Vo <quevo.job@gmail.com>
2025-08-20 12:53:48 +1000

    Clean the code and organize files

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-
+const MONGO_URI = process.env.MONGO_URI;
 async function connect(){
     try {
-        await mongoose.connect('mongodb://127.0.0.1/Hannah_education_dev');
+        // await mongoose.connect('mongodb://127.0.0.1/Hannah_education_dev');
+        await mongoose.connect(MONGO_URI);
         console.log('Connect Successfully!!')
-
     } catch (error) {
         console.log("Fail")
     }
